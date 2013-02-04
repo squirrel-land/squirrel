e85ef3fa90983713edc77397741ecd25ee3ea40c
Song Gao <song@gao.io>
2013-02-03 21:26:26 -0600

    gofmt

--- simpleModels/September2nd.go
+++ simpleModels/September2nd.go
@@ -9,19 +9,19 @@ import (
 
 type september2nd struct {
 	noDeliveryDistance float64
-    interferenceRange float64
+	interferenceRange  float64
 
-	nodes      []*modelDep.Position
-	buckets    []*leakyBucket // measured by bytes of data; if non-maxDataRate is used, value added into buckets is the number of bytes that could be sent on maxDataRate within the same amount of time
-	dataRates   []float64 // supported data rates in Mbps in increasing order
-    maxDataRate float64
+	nodes       []*modelDep.Position
+	buckets     []*leakyBucket // measured by bytes of data; if non-maxDataRate is used, value added into buckets is the number of bytes that could be sent on maxDataRate within the same amount of time
+	dataRates   []float64      // supported data rates in Mbps in increasing order
+	maxDataRate float64
 }
 
 func NewSeptember2nd() modelDep.September {
 	ret := new(september2nd)
 	ret.dataRates = []float64{6, 9, 12, 18, 24, 36, 48, 54}
-    ret.maxDataRate = ret.dataRates[len(ret.dataRates)-1]
-    return ret
+	ret.maxDataRate = ret.dataRates[len(ret.dataRates)-1]
+	return ret
 }
 
 func (september *september2nd) ParametersHelp() string {
@@ -30,14 +30,14 @@ func (september *september2nd) ParametersHelp() string {
 
 func (september *september2nd) Configure(config map[string]interface{}) (err error) {
 	dist, okDist := config["LowestZeroPacketDeliveryDistance"].(float64)
-    iRange, okIRange := config["InterferenceRange"].(float64)
+	iRange, okIRange := config["InterferenceRange"].(float64)
 
 	if true != (okDist && okIRange) {
 		return ParametersNotValid
 	}
 
 	september.noDeliveryDistance = dist
-    september.interferenceRange = iRange
+	september.interferenceRange = iRange
 
 	return nil
 }
@@ -46,92 +46,92 @@ func (september *september2nd) Initialize(nodes []*modelDep.Position) {
 	september.nodes = nodes
 	september.buckets = make([]*leakyBucket, len(nodes), len(nodes))
 	for it := range september.buckets {
-        september.buckets[it] = &leakyBucket{
-            BucketSize: int32(1024 * 1024 * september.maxDataRate),
-            OutResolution: int32(10),
-            OutPerMilliSecond: int32(1024 * 1024 * september.maxDataRate / 1000),
-        }
-        september.buckets[it].Go()
+		september.buckets[it] = &leakyBucket{
+			BucketSize:        int32(1024 * 1024 * september.maxDataRate),
+			OutResolution:     int32(10),
+			OutPerMilliSecond: int32(1024 * 1024 * september.maxDataRate / 1000),
+		}
+		september.buckets[it].Go()
 	}
 }
 
 func (september *september2nd) SendUnicast(source int, destination int, size int) bool {
-    p1 := september.nodes[source]
-    p2 := september.nodes[destination]
-    if p1 == nil || p2 == nil {
-        return false
-    }
-    dist := distance(p1, p2)
+	p1 := september.nodes[source]
+	p2 := september.nodes[destination]
+	if p1 == nil || p2 == nil {
+		return false
+	}
+	dist := distance(p1, p2)
 
-    // Go through source bucket
-    if !september.buckets[source].In(size) {
-        return false
-    }
+	// Go through source bucket
+	if !september.buckets[source].In(size) {
+		return false
+	}
 
-    // Since the packet is out in the air, interference should be put on neighbor nodes
-    for i := 1; i < len(september.nodes); i++ {
-        if i != source && i != destination && dist < september.interferenceRange {
-            september.buckets[i].In(size)
-        }
-    }
+	// Since the packet is out in the air, interference should be put on neighbor nodes
+	for i := 1; i < len(september.nodes); i++ {
+		if i != source && i != destination && dist < september.interferenceRange {
+			september.buckets[i].In(size)
+		}
+	}
 
-    // The packet takes the adventure in the air (fading, etc.)
-    if dist > september.noDeliveryDistance * 0.85 {
-        if rand.Float64() < math.Pow(dist/september.noDeliveryDistance, 5) {
-            return false
-        }
-    }
+	// The packet takes the adventure in the air (fading, etc.)
+	if dist > september.noDeliveryDistance*0.85 {
+		if rand.Float64() < math.Pow(dist/september.noDeliveryDistance, 5) {
+			return false
+		}
+	}
 
-    // Go through destination bucket
-    if !september.buckets[destination].In(size) {
-        return false
-    }
+	// Go through destination bucket
+	if !september.buckets[destination].In(size) {
+		return false
+	}
 
-    // The packet is gonna be delivered!
-    return true
+	// The packet is gonna be delivered!
+	return true
 }
 
 func (september *september2nd) SendBroadcast(source int, size int, underlying []int) []int {
-    p1 := september.nodes[source]
-    if p1 == nil {
-        return underlying[:0]
-    }
+	p1 := september.nodes[source]
+	if p1 == nil {
+		return underlying[:0]
+	}
 
-    // Go through source bucket
-    if !september.buckets[source].In(size) {
-        return underlying[:0]
-    }
+	// Go through source bucket
+	if !september.buckets[source].In(size) {
+		return underlying[:0]
+	}
 
-    count := 0
-    for i := 1; i < len(september.nodes); i++ {
-        p2 := september.nodes[i]
-        if p2 == nil {
-            continue
-        }
-        dist := distance(p1, p2)
+	count := 0
+	for i := 1; i < len(september.nodes); i++ {
+		p2 := september.nodes[i]
+		if p2 == nil {
+			continue
+		}
+		dist := distance(p1, p2)
 
-        // Since the packet is out in the air, interference should be put on neighbor nodes
-        if dist < september.interferenceRange {
-            if !september.buckets[i].In(size) {
-                // Go through destination bucket. If rejected by the bucket, the broadcasted packet should not be delivered to this node
-                continue
-            }
-        }
+		// Since the packet is out in the air, interference should be put on neighbor nodes
+		if dist < september.interferenceRange {
+			if !september.buckets[i].In(size) {
+				// Go through destination bucket. If rejected by the bucket, the broadcasted packet should not be delivered to this node
+				continue
+			}
+		}
 
-        // The packet takes the adventure in the air (fading, etc.). There's still a possibility the packet is not delivered to this node
-        if dist > september.noDeliveryDistance * 0.85 {
-            if rand.Float64() < math.Pow(dist/september.noDeliveryDistance, 5) {
-                continue
-            }
-        }
+		// The packet takes the adventure in the air (fading, etc.). There's still a possibility the packet is not delivered to this node
+		if dist > september.noDeliveryDistance*0.85 {
+			if rand.Float64() < math.Pow(dist/september.noDeliveryDistance, 5) {
+				continue
+			}
+		}
 
-        // The packet is gonna be delivered!
-        underlying[count] = i
-        count++
-    }
-    return underlying[:count]
+		// The packet is gonna be delivered!
+		underlying[count] = i
+		count++
+	}
+	return underlying[:count]
 }
 
 func (september *september2nd) dataRate(src, dst int) float64 {
-    return september.dataRates[len(september.dataRates) - 1]
+	return september.dataRates[len(september.dataRates)-1]
 }
--- simpleModels/leakyBucket.go
+++ simpleModels/leakyBucket.go
@@ -1,41 +1,42 @@
 package simpleModels
 
 import (
-    "time"
-    "sync/atomic"
+	"sync/atomic"
+	"time"
 )
 
 // Thread-safe leaky bucket
 type leakyBucket struct {
-    BucketSize int32
-    OutResolution int32 // in milliseconds
-    OutPerMilliSecond int32
+	BucketSize        int32
+	OutResolution     int32 // in milliseconds
+	OutPerMilliSecond int32
 
-    bucket int32
-    started bool
+	bucket  int32
+	started bool
 }
+
 // by far (before Go 1.1), int is 32 bit no matter what arch it is. After Go 1.1 release, int64 should be used here, since int is going to be 64 bit on 64 bit arch and 64 bit machine would be the most common platform that Master runs on.
 
 func (this *leakyBucket) In(size int) bool {
-    if atomic.LoadInt32(&this.bucket) > this.BucketSize {
-        return false
-    }
-    atomic.AddInt32(&this.bucket, int32(size))
-    return true
+	if atomic.LoadInt32(&this.bucket) > this.BucketSize {
+		return false
+	}
+	atomic.AddInt32(&this.bucket, int32(size))
+	return true
 }
 
 func (this *leakyBucket) Go() {
-    go func() {
-        sleepTime := time.Duration(this.OutResolution) * time.Millisecond
-        for {
-            if atomic.LoadInt32(&this.bucket) > 0 {
-                atomic.AddInt32(&this.bucket, -int32(atomic.LoadInt32(&this.OutPerMilliSecond) * this.OutResolution))
-            }
-            time.Sleep(sleepTime)
-        }
-    }()
+	go func() {
+		sleepTime := time.Duration(this.OutResolution) * time.Millisecond
+		for {
+			if atomic.LoadInt32(&this.bucket) > 0 {
+				atomic.AddInt32(&this.bucket, -int32(atomic.LoadInt32(&this.OutPerMilliSecond)*this.OutResolution))
+			}
+			time.Sleep(sleepTime)
+		}
+	}()
 }
 
 func (this *leakyBucket) UpdateOutRate(outPerMilliSecond int) {
-    atomic.StoreInt32(&this.OutPerMilliSecond, int32(outPerMilliSecond))
+	atomic.StoreInt32(&this.OutPerMilliSecond, int32(outPerMilliSecond))
 }
